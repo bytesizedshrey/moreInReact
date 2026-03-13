a4a93333523abfcacbcaecb13ec43600ee630808
bytesizedshrey <shreyash18gajbhiye@icloud.com>
2026-03-13 17:24:58 +0530

    first commit

---
+++ moreInReact/src/App.jsx
@@ -0,0 +1,17 @@
+import Cards from "./components/Card";
+
+const App = () => {
+  const age = 10;
+  var user = 'shreyash'
+  const arr = ["emily","rose","sabrina","taylor"]
+  return <div className="text-xl text-red-900">
+    <h1>{user},{age}</h1>
+    <h6>{arr.map(function(elem,id){
+      return <h1>{elem},{id}</h1> //modified and returned
+    })}</h6>
+    <Cards/>
+  </div>;
+};
+ 
+export default App;
+  
---
+++ moreInReact/src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div>
+      This is From the Card Compnent
+    </div>
+  )
+}
+
+export default Cards
