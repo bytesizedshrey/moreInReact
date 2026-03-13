ef9d85c15fb4a5c0126357926b2c61fc3016d6bf
bytesizedshrey <shreyash18gajbhiye@icloud.com>
2026-03-13 18:58:23 +0530

    update 6:58pm

--- moreInReact/src/App.jsx
+++ moreInReact/src/App.jsx
@@ -1,17 +1,19 @@
 import Cards from "./components/Card";
 
 const App = () => {
-  const age = 10;
-  var user = 'shreyash'
-  const arr = ["emily","rose","sabrina","taylor"]
-  return <div className="text-xl text-red-900">
-    <h1>{user},{age}</h1>
+  // const age = 10;
+  // var user = 'shreyash'
+  // const arr = ["emily","rose","sabrina","taylor"]
+  return (
+    <div className="text-xl text-red-900">
+      <Cards user= "virat" />
+      {/* {Cards('shreyash', 22)} */}
+      {/* <h1>{user},{age}</h1>
     <h6>{arr.map(function(elem,id){
       return <h1>{elem},{id}</h1> //modified and returned
-    })}</h6>
-    <Cards/>
-  </div>;
+    })}</h6> */}
+    </div>
+  );
 };
- 
+
 export default App;
-  
--- moreInReact/src/components/Card.jsx
+++ moreInReact/src/components/Card.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const Cards = () => {
-  return (
-    <div>
-      This is From the Card Compnent
-    </div>
+const Cards = ({user}) => {
+      // console.log(props)
+  return (    
+      <div className="bg-amber-50 rounded-2xl px-5 py-3 w-fit">
+        <h1 className="text-4xl font-semibold">{user}</h1>
+      </div>
+    
   )
 }
 
 export default Cards
+ 
